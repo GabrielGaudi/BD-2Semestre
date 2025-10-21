62351c18da98d2c8a3c76cb5c1be0fdcfaef7c34
GabrielGaudi <162371856+GabrielGaudi@users.noreply.github.com>
2025-10-21 20:23:54 -0300

    Add files via upload

---
+++ DMLex1.sql
@@ -0,0 +1,81 @@
+use dbdistribuidora;
+
+insert into tbfornecedor(Codigo, Nome, CNPJ, Telefone) values
+(1, "Revenda Chico Loco", 1245678937123, 11934567897),
+(2, "José Faz Tudo S/A", 1345678937123, 11934567898),
+(3, "Vadalto Entregas", 1445678937123, 11934567899),
+(4, "Astrogildo das Estrelas", 1545678937123, 11934567800),
+(5, "Amoroso e Doce", 1645678937123, 11934567801),
+(6, "Marcelo Dedal", 1745678937123, 1193457802),
+(7, "Franciscano Cachaça", 1845678937123, 11934567803),
+(8, "Joãozinho Chupeta", 1945678937123, 11934567804);
+
+Delimiter $$
+create procedure inserirCidade(Id int, Cidade varchar(200))
+begin
+insert into tbcidade(CidadeId, Cidade) value(Id, Cidade);
+end $$
+
+call inserirCidade(1,"Rio de Janeiro");
+call inserirCidade(2,"São Carlos");
+call inserirCidade(3,"Campinas");
+call inserirCidade(4,"Franco da Rocha");
+call inserirCidade(5,"Osasco");
+call inserirCidade(6,"Pirituba");
+call inserirCidade(7,"Lapa");
+call inserirCidade(8,"Ponta Grossa");
+
+
+Delimiter $$
+create procedure inserirEstado(Id int, UF char(2))
+begin
+insert into tbestado(UFId, UF) value(Id, UF);
+end $$
+
+
+call inserirEstado(1,"SP");
+call inserirEstado(2,"RJ");
+call inserirEstado(3,"RS");
+
+Delimiter $$
+create procedure inserirBairro(Id int, Bairro varchar(200))
+begin
+insert into tbbairro(BairroId, Bairro) value(Id, Bairro);
+end $$
+
+drop procedure inserirProduto;
+
+call inserirBairro(1,"Aclimação");
+call inserirBairro(2,"Capão Redondo");
+call inserirBairro(3,"Pirituba");
+call inserirBairro(4,"Liberdade");
+
+Delimiter $$
+create procedure inserirProduto(CodigoBarras decimal(14,0), Nome varchar(200), Valor decimal(8,2), Qtd int)
+begin
+insert into tbproduto(CodigoBarras, Nome, Valor, Qtd) value(CodigoBarras, Nome, Valor, Qtd);
+end $$
+
+call inserirProduto(12345678910111, "Rei de Papel Mache",  54.61, 120);
+call inserirProduto(12345678910112, "Bolinha de Sabão", 100.45, 120);
+call inserirProduto(12345678910113, "Carro Bate",  44.00, 120);
+call inserirProduto(12345678910114, "Bola Furada",  10.00, 120);
+call inserirProduto(12345678910115, "Maçã Laranja", 99.44, 120);
+call inserirProduto(12345678910116, "Boneco do Hitler", 124.00, 200);
+call inserirProduto(12345678910117, "Farinha de Suruí",  50.00, 200);
+call inserirProduto(12345678910118, "Zelador de Cemitério",  24.50, 100);
+
+Delimiter $$
+create procedure inserirEndereco(Logradouro varchar(200), CEP decimal(8,0), BairroId int, CidadeId int, UFId int)
+begin
+insert into tbendereco(Logradouro, CEP, BairroId, CidadeId, UFId) value(Logradouro, CEP, BairroId, CidadeId, UFId);
+end $$
+
+call inserirEndereco("Rua da Federal ", , , , );
+call inserirEndereco("", , , , );
+call inserirEndereco("", , , , );
+call inserirEndereco("", , , , );
+call inserirEndereco("", , , , );
+call inserirEndereco("", , , , );
+call inserirEndereco("", , , , );
+call inserirEndereco("", , , , );
